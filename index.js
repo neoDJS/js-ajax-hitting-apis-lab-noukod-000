435833f0470cd08088e582dbd8cdff7b5e67dc6b
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-12 16:57:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,16 +60,16 @@ function displayCommits() {
 }
 
 function displayBranches() {
-  const commits = JSON.parse(this.responseText);
-  const commitsList = `<ul>${commits
+  const branches = JSON.parse(this.responseText);
+  const branchesList = `<ul>${branches
     .map(
-      commit =>
+      branche =>
         '<li><strong>' +
-        commit.author.login +
+        branche.author.login +
         '</strong> - ' +
-        commit.commit.message +
+        branche.commit.message +
         '</li>'
     )
     .join('')}</ul>`;
-  document.getElementById('commits').innerHTML = commitsList;
+  document.getElementById('commits').innerHTML = branchesList;
 }
