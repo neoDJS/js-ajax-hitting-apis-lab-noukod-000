30c2af382677aba07fd430734f56c1164c2a58b2
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-12 16:54:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,12 +29,27 @@ function showRepositories() {
 function getCommits(el) {
   const name = el.dataset.repo;
   const req = new XMLHttpRequest();
-  req.addEventListener('load', showCommits);
+  req.addEventListener('load', displayCommits);
   req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
   req.send();
 }
 
-function showCommits() {
+function displayCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('commits').innerHTML = commitsList;
+}
+
+function displayBranches() {
   const commits = JSON.parse(this.responseText);
   const commitsList = `<ul>${commits
     .map(
