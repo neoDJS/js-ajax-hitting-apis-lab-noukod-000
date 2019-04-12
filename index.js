94a04af4d9fea64bf3e73c2af7e6976ccb4dcba9
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-12 16:48:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function getRepositories() {
+  const username = document.getElementById('username').value;
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories);
-  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.open('GET', 'https://api.github.com/users/'++'/repos');
   req.send();
 }
 
