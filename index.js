ee64c487630a87c54a1f86cc009af97fe9b19ace
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-12 16:49:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function getRepositories() {
   const username = document.getElementById('username').value;
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories);
-  req.open('GET', 'https://api.github.com/users/'++'/repos');
+  req.open('GET', 'https://api.github.com/users/'+username+'/repos');
   req.send();
 }
 
