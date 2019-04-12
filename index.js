ec9fdfeda19dec01b619bb1ea6e3165efe728cd6
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-12 17:16:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function getRepositories() {
   const username = document.getElementById('username').value;
   const req = new XMLHttpRequest();
-  req.addEventListener('load', showRepositories);
+  req.addEventListener('load', displayRepositories);
   req.open('GET', 'https://api.github.com/users/'+username+'/repos');
   req.send();
 }
 
-function showRepositories() {
+function displayRepositories() {
   //this is set to the XMLHttpRequest object that fired the event
   var repos = JSON.parse(this.responseText);
   console.log(repos);
